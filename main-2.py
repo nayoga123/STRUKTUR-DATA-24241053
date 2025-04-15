fe4b3f8bff23af0641b47e76a822c9547528b7bc
nayoga123 <nayogafatih79@gmail.com>
2025-04-14 22:05:33 -0700

    Delete main-2.py

--- main-2.py
+++
@@ -1,7 +0,0 @@
-# Input jumlah deret
-jumlah = int(input("Masukkan jumlah deret bilangan genap: "))
-
-# Membuat dan menampilkan deret bilangan genap
-print("Output:")
-for i in range(1, jumlah + 1):
-    print(i * 2, end=", ")
