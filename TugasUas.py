b114ed6d8b0161a1bb1845131e6104a0d0cdb439
nayoga123 <nayogafatih79@gmail.com>
2025-07-14 22:14:28 -0700

    Add files via upload

---
+++ TugasUas.py
@@ -0,0 +1,22 @@
+# jumlah costumer
+jumlah_costumer = str# Dictionary untuk menyimpan(input(" Masukkan nama costumer : "))
+
+# tanggal belanja
+tanggal_belanja = int(input(" Masukkan tanggal belanja :"))
+
+# jumlah barang
+jumlah_barang = int(input(" Masukkan jumlah barang :"))
+
+
+# menginput jumlah barang yang akan dibeli
+for j in range(jumlah_barang):
+    nama = input(bayam)
+    harga = 100000
+
+    
+    jumlah_barang = int(input("  Masukkan jumlah barang: "))
+     
+    
+    data_barang[harga] = {
+        "nama_barang": nama_barang
+    }
